39dfcfc3d36b710f1e06c8e3b1b184297d188acb
promocopy <tobijosh4@gmail.com>
2023-09-10 20:48:11 +0100

    initial commit

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -14,17 +14,31 @@ namespace istTask.Controllers
         {
             var user = new
             {
-                slackName = "Promocopy",
-                currentDay = DateTime.UtcNow.DayOfWeek,
-                utcTime = DateTime.UtcNow.ToString("yyyy-mm-dd:hh-mm-ssZ"),
-                track = "Backend",
+                slack_name = slack_name,
+                current_Day = DateTime.UtcNow.DayOfWeek.ToString(),  
+                utc_time = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                track = track.ToLower(),
                 github_file_url= "https://github.com/Promocopy/istTask/blob/master/istTask/Controllers/TaskController.cs",
                 github_repo_url = "https://github.com/Promocopy/istTask",
-                StatusCode = Response.StatusCode
+                status_code = Response.StatusCode
 
 
             };
             return Ok(user);
+
+            //{ "slackName":"Promocopy",
+            //"currentDay":0,
+            //"utcTime":"2023-24-10:07-24-44Z",
+            //"track":"Backend",
+            //"github_file_url":"https://github.com/Promocopy/istTask/blob/master/istTask/Controllers/TaskController.cs",
+
+            //"github_repo_url":"https://github.com/Promocopy/istTask",
+
+            //"statusCode":200}
+
+
+
+
         }
     }
 }
