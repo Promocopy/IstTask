ed7203c45b8c7f0544e01c6b9f9a162bd40e982e
promocopy <tobijosh4@gmail.com>
2023-09-10 20:06:40 +0100

    initial commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace istTask.Controllers
+{
+    
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api")]
+        public IActionResult Task(string slack_name, string track)
+        {
+            var user = new
+            {
+                slackName = "Promocopy",
+                currentDay = DateTime.UtcNow.DayOfWeek,
+                utcTime = DateTime.UtcNow.ToString("yyyy-mm-dd:hh-mm-ssZ"),
+                track = "Backend",
+                github_file_url= "https://github.com/Promocopy/istTask/blob/master/istTask/Controllers/TaskController.cs",
+                github_repo_url = "https://github.com/Promocopy/istTask",
+                StatusCode = Response.StatusCode
+
+
+            };
+            return Ok(user);
+        }
+    }
+}
---
+++ Entity/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace istTask.Entity
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? slackName { get; set; }
+        public DateTime creationDate { get; set; }
+        public DayOfWeek Day { get; set; } = DateTime.Today.DayOfWeek;
+        public string? Track { get; set; }
+        public int statuscode { get; set; }
+    }
+}
